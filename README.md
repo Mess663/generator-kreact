31672591873a997f84b46ccb5615d854eda4b7d7
liukunhui <liukunhui@youcheyihou.com>
2020-08-09 22:17:45 +0800

    feat: 完成生成逻辑

---
+++ README.md
@@ -0,0 +1,9 @@
+## generator-kreact
+
+使用方法：
+
+`npm install -g yo`
+
+`npm install -g generator-kreact`
+
+`yo kreact`
--- index.js
+++ index.js
@@ -1,61 +1,47 @@
-const Generator = require('yeoman-generator')
+const Generator = require('yeoman-generator');
 
-const fs = require('fs')
-
-console.log('jinllllll')
+const fs = require('fs');
 
 module.exports = class extends Generator {
-  // prompting() {
-  //   return this.prompt([
-  //     {
-  //       type: 'input',
-  //       name: 'appName',
-  //       message: '项目名称：',
-  //       validate(input) {
-  //         if (!input) return '请输入项目名称'
-  //         if (fs.readdirSync('.').includes(input)) {
-  //           return '目录已存在'
-  //         }
-  //         return true
-  //       },
-  //     },
-  //     {
-  //       type: 'list',
-  //       choices: ['Javascript', 'TypeScript'],
-  //       name: 'language',
-  //       message: '项目语言',
-  //       default: 'TypeScript',
-  //     },
-  //   ]).then(answers => {
-  //     this.answers = answers
-  //   })
-  // }
-
-  // writing() {
-  //   console.log(this.answers)
-  // }
-
-  // end() {
-  //   this.log('happy coding!')
-  // }
-
-  constructor(args, opts) {
-    super(args, opts);
-
-    console.log(args, opts)
-  
-    // This makes `appname` a required argument.
-    this.argument("appname", { type: String, required: true });
-
-    // And you can then access it later; e.g.
-    this.log(this.options.appname);
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'appName',
+        message: '项目名称：',
+        validate(input) {
+          if (!input) return '请输入项目名称';
+          if (fs.readdirSync('.').includes(input)) {
+            return '目录已存在';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'list',
+        choices: ['kbone'],
+        name: 'language',
+        message: '项目语言',
+        default: 'kbone',
+      },
+    ]).then((answers) => {
+      this.answers = answers;
+    });
   }
 
-  method1() {
-    console.log('method 1')
+  writing() {
+    const { language, appName } = this.answers;
+    // copyTpl 会使用模板引擎，替换 <%= appName %>
+    this.fs.copyTpl(this.templatePath(language, '_package.json'), this.destinationPath(appName, 'package.json'), this.answers);
+    // copy 支持文件/文件夹
+    fs.readdirSync(this.templatePath(language))
+      .filter((name) => !name.startsWith('_'))
+      .forEach((item) => {
+        this.fs.copy(this.templatePath(language, item), this.destinationPath(appName, item));
+      });
   }
 
-  method2() {
-    console.log('method 2')
+  end() {
+    this.log('脚手架生成完毕！');
   }
-}
+};
